76a789674855cef4a8295e9be3a256d9e4a15df5
Itadori <129357662+CyberPvt@users.noreply.github.com>
2023-05-26 17:17:55 +0800

    Sales Issues

--- ViewTotal.php
+++ ViewTotal.php
@@ -1,48 +1,85 @@
 <?php
-$page_title = 'TotalPerformances';
+$page_title = 'View Total Performance';
 include('./includes/header.html');
 ?>
 
-<h1>Best Performances by Agents</h1>
+<h1>View Data</h1>
+
+<form action="ViewTotal.php" method="post">
+    <label for="filter">Filter by:</label>
+    <select name="filter" id="filter">
+        <option value="Agents" <?php if(isset($_POST['filter']) && $_POST['filter'] == "Agents") echo "selected"; ?>>Agents</option>
+        <option value="Products" <?php if(isset($_POST['filter']) && $_POST['filter'] == "Products") echo "selected"; ?>>Products</option>
+        <option value="AgentsAndProducts" <?php if(isset($_POST['filter']) && $_POST['filter'] == "AgentsAndProducts") echo "selected"; ?>>Agents and Products</option>
+    </select>
+    <button type="submit">View</button>
+</form>
 
 <?php
-require_once('mysqli.php');
-global $dbc;
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['filter'])) {
+    require_once('mysqli.php');
+    global $dbc;
 
-$query = "SELECT agents.agent_name, SUM(orders.order_quantity) AS total_quantity, SUM(orders.order_quantity * products.price) AS total_price, SUM(sales.profit) AS total_profit
-          FROM agents
-          INNER JOIN orders ON agents.agent_id = orders.agent_id
-          INNER JOIN products ON orders.product_id = products.product_id
-          INNER JOIN sales ON orders.order_id = sales.order_id
-          GROUP BY agents.agent_id
-          ORDER BY total_price DESC";
+    $filter = $_POST['filter'];
 
-$result = $dbc->query($query);
-
-if ($result->num_rows > 0) {
-    echo '<table>
-            <tr>
-                <th>Agent Name</th>
-                <th>Total Quantity</th>
-                <th>Total Price</th>
-                <th>Total Profit</th>
-            </tr>';
-
-    while ($row = $result->fetch_assoc()) {
-        echo '<tr>
-                <td>' . $row['agent_name'] . '</td>
-                <td>' . $row['total_quantity'] . '</td>
-                <td>' . $row['total_price'] . '</td>
-                <td>' . $row['total_profit'] . '</td>
-            </tr>';
+    if ($filter == "Agents") {
+        $query = "SELECT agents.agent_name, products.product_name, SUM(orders.order_quantity) AS total_quantity
+                  FROM agents
+                  INNER JOIN orders ON agents.agent_id = orders.agent_id
+                  INNER JOIN products ON orders.product_id = products.product_id
+                  GROUP BY agents.agent_name
+                  ORDER BY total_quantity DESC LIMIT 1";
+        $title = "Agent with Highest Sale";
+        $agentColumn = true;
+    } elseif ($filter == "Products") {
+        $query = "SELECT products.product_name, SUM(orders.order_quantity) AS total_quantity
+                  FROM products
+                  INNER JOIN orders ON products.product_id = orders.product_id
+                  GROUP BY products.product_name
+                  ORDER BY total_quantity DESC";
+        $title = "Products with Highest Sold";
+        $agentColumn = false;
+    } elseif ($filter == "AgentsAndProducts") {
+        $query = "SELECT agents.agent_name, products.product_name, SUM(orders.order_quantity) AS total_quantity
+                  FROM agents
+                  INNER JOIN orders ON agents.agent_id = orders.agent_id
+                  INNER JOIN products ON orders.product_id = products.product_id
+                  GROUP BY agents.agent_name, products.product_name
+                  ORDER BY total_quantity DESC LIMIT 1";
+        $title = "Agent and Product with Highest Sale";
+        $agentColumn = true;
     }
 
-    echo '</table>';
-} else {
-    echo '<p>No data available.</p>';
+    $result = $dbc->query($query);
+
+    if ($result->num_rows > 0) {
+        echo '<h2>' . $title . '</h2>';
+        echo '<table>
+                <tr>';
+        if ($agentColumn) {
+            echo '<th>Agent Name</th>';
+        }
+        echo '<th>Product Name</th>
+                    <th>Total Quantity</th>
+                </tr>';
+
+        while ($row = $result->fetch_assoc()) {
+            echo '<tr>';
+            if ($agentColumn) {
+                echo '<td>' . $row['agent_name'] . '</td>';
+            }
+            echo '<td>' . $row['product_name'] . '</td>
+                  <td>' . $row['total_quantity'] . '</td>
+                </tr>';
+        }
+
+        echo '</table>';
+    } else {
+        echo '<p>No data available.</p>';
+    }
+
+    mysqli_close($dbc);
 }
 
-mysqli_close($dbc);
+include('./includes/footer.html');
 ?>
-
-<?php include('./includes/footer.html'); ?>
