a8d55c0f7b99970b108591f057ca16f92652097e
Pythoenixx <azanyzikry25@gmail.com>
2023-05-27 15:22:45 +0800

    tag main

--- Logout.php
+++ Logout.php
@@ -2,6 +2,7 @@
 $page_title = 'Logout';
 ?>
 <link rel="stylesheet" type="text/css" href="includes/efek.css" />
-<h1>You have been logout ! </h1>
-<h2>Thank You for using us :></h2>
-
+<main>
+    <h1>You have been logout ! </h1>
+    <h2>Thank You for using us :></h2>
+</main>
