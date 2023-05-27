5c6b940d15cad593b8498a8a58b84107c9f55d11
Pythoenixx <azanyzikry25@gmail.com>
2023-05-27 15:15:41 +0800

    buang bartepi drpd logout(improve security)

--- Logout.php
+++ Logout.php
@@ -1,11 +1,7 @@
 <?php
 $page_title = 'Logout';
-include('./includes/header.html');
 ?>
 
 <h1>You have been logout ! </h1>
 <h2>Thank You for using us :></h2>
 
-<?php
-include('./includes/footer.html');
-?>
