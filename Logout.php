30379578122f4fc5f855ef571fa1b29310cccf9f
Pythoenixx <azanyzikry25@gmail.com>
2023-05-27 19:42:50 +0800

    add auto redirect to login after some seconds

--- Logout.php
+++ Logout.php
@@ -5,4 +5,8 @@ $page_title = 'Logout';
 <main>
     <h1>You have been logout ! </h1>
     <h2>Thank You for using us :></h2>
+<h5>You will be redirected to the login page in 6.9 seconds</h5>
 </main>
+<?php
+header('Refresh: 6.9; MainPage.php'); 
+?>
