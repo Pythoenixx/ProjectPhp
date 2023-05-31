ed0c552c06c3fd40e61220de1f75d2ef8f29cae7
Itadori <129357662+CyberPvt@users.noreply.github.com>
2023-05-31 22:57:45 +0800

    few changes

--- Register.php
+++ Register.php
@@ -15,6 +15,12 @@ if (isset($_POST['submitted'])) {
         $errors[] = 'You forgot to enter your username.';
     } else {
         $username = $_POST['username'];
+        // Check for a unique username.
+        $query = "SELECT user_id FROM users WHERE username='$username'";
+        $result = mysqli_query($dbc, $query);
+        if (mysqli_num_rows($result) > 0) {
+            $errors[] = 'The username is already taken. Please choose a different username.';
+        }
     }
 
     // Check for a password and match against the confirmed password.
@@ -46,54 +52,45 @@ if (isset($_POST['submitted'])) {
 
         // Register the user in the database.
 
-        // Check for previous registration.
-        $query = "SELECT user_id FROM users WHERE username='$username'";
-        $result = @mysqli_query($dbc, $query); // Run the query.
-        if (mysqli_num_rows($result) == 0) {
+        // Insert into users table.
+        $query = "INSERT INTO users (username, password, email, phone_number, registration_date, role) 
+                  VALUES ('$username', '$password', '$email', '$phone_number', NOW(), 'supplier')";
+        $result = mysqli_query($dbc, $query); // Run the query.
+        if ($result) { // If it ran OK.
 
-            // Insert into users table.
-            $query = "INSERT INTO users (username, password, email, phone_number, registration_date, role) 
-                      VALUES ('$username', '$password', '$email', '$phone_number', NOW(), 'supplier')";
-            $result = @mysqli_query($dbc, $query); // Run the query.
-            if ($result) { // If it ran OK.
+            // Get the last inserted user_id.
+            $user_id = mysqli_insert_id($dbc);
 
-                // Get the last inserted user_id.
-                $user_id = mysqli_insert_id($dbc);
+            // Insert into suppliers table.
+            $query = "INSERT INTO suppliers (supplier_name, contact_email, contact_phone) 
+                      VALUES ('$username', '$email', '$phone_number')";
+            $result = mysqli_query($dbc, $query);
 
-                // Insert into supplier table.
-                $query = "INSERT INTO suppliers (supplier_name, contact_email, contact_phone) 
-                          VALUES ('$username', '$email', '$phone_number')";
-                $result = mysqli_query($dbc, $query);
+            if ($result) {
+                // Get the inserted supplier_id.
+                $supplier_id = mysqli_insert_id($dbc);
 
-                if ($result) {
-                    // Get the inserted supplier_id.
-                    $supplier_id = mysqli_insert_id($dbc);
+                // Print a success message.
+                echo '<h1 id="mainhead">Thank you!</h1>
+                <p>You are now registered as a supplier.</p><p><br /></p>';
 
-                    // Print a success message.
-                    echo '<h1 id="mainhead">Thank you!</h1>
-                    <p>You are now registered as a supplier.</p><p><br /></p>';
-
-                    // Include the footer and quit the script (to not show the form).
-                    include('./includes/footer.html');
-                    exit();
-                } else {
-                    // If inserting into the supplier table failed.
-                    echo '<h1 id="mainhead">System Error</h1>
-                    <p class="error">You could not be registered due to a system error. We apologize for any inconvenience.</p>'; // Public message.
-                    echo '<p>' . mysqli_error($dbc) . '<br /><br />Query: ' . $query . '</p>'; // Debugging message.
-                    include('./includes/footer.html');
-                    exit();
-                }
-            } else { // If inserting into the users table failed.
+                // Include the footer and quit the script (to not show the form).
+                include('./includes/footer.html');
+                exit();
+            } else {
+                // If inserting into the suppliers table failed.
                 echo '<h1 id="mainhead">System Error</h1>
                 <p class="error">You could not be registered due to a system error. We apologize for any inconvenience.</p>'; // Public message.
                 echo '<p>' . mysqli_error($dbc) . '<br /><br />Query: ' . $query . '</p>'; // Debugging message.
                 include('./includes/footer.html');
                 exit();
             }
-        } else { // Already registered.
-            echo '<h1 id="mainhead">Error!</h1>
-            <p class="error">The username has already been registered.</p>';
+        } else { // If inserting into the users table failed.
+            echo '<h1 id="mainhead">System Error</h1>
+            <p class="error">You could not be registered due to a system error. We apologize for any inconvenience.</p>'; // Public message.
+            echo '<p>' . mysqli_error($dbc) . '<br /><br />Query: ' . $query . '</p>'; // Debugging message.
+            include('./includes/footer.html');
+            exit();
         }
     } else { // Report the errors.
         echo '<h1 id="mainhead">Error!</h1>
@@ -111,23 +108,23 @@ if (isset($_POST['submitted'])) {
     <h2>Register as Supplier</h2>
     <form action="register.php" method="post">
         <div class="user-box">
-            <input type="text" name="username" required="" maxlength="15" value="<?php if (isset($_POST['username'])) echo $_POST['username']; ?>" /></p>
+            <input type="text" name="username" required maxlength="15" value="<?php if (isset($_POST['username'])) echo $_POST['username']; ?>" />
             <label>Username</label>
         </div>
         <div class="user-box">
-            <input type="password" name="password1" required="" maxlength="20" />
+            <input type="password" name="password1" required maxlength="20" />
             <label>Password</label>
         </div>
         <div class="user-box">
-            <input type="password" name="password2" required="" maxlength="20" />
+            <input type="password" name="password2" required maxlength="20" />
             <label>Confirm Password</label>
         </div>
         <div class="user-box">
-            <input type="text" name="email" required="" maxlength="40" value="<?php if (isset($_POST['email'])) echo $_POST['email']; ?>" /></p>
+            <input type="text" name="email" required maxlength="40" value="<?php if (isset($_POST['email'])) echo $_POST['email']; ?>" />
             <label>Email</label>
         </div>
         <div class="user-box">
-            <input type="text" name="phone_number" required="" maxlength="15" value="<?php if (isset($_POST['phone_number'])) echo $_POST['phone_number']; ?>" /></p>
+            <input type="text" name="phone_number" required maxlength="15" value="<?php if (isset($_POST['phone_number'])) echo $_POST['phone_number']; ?>" />
             <label>Phone Number</label>
         </div>
         <div class="butang">
@@ -135,12 +132,11 @@ if (isset($_POST['submitted'])) {
             <span></span>
             <span></span>
             <span></span>
-            <input type="submit" name="submit" value="Register" class="butang-teks"/>
+            <input type="submit" name="submit" value="Register" class="butang-teks" />
         </div>
         <input type="hidden" name="submitted" value="TRUE" />
+    </form>
 </div>
-
-</form>
 <?php
 include('./includes/footer.html');
 ?>
