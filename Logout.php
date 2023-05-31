6ea492c71c6b0dd75e53862a3b77309512410356
Itadori <129357662+CyberPvt@users.noreply.github.com>
2023-06-01 02:52:42 +0800

    logout asso

--- Logout.php
+++ Logout.php
@@ -6,11 +6,11 @@ $page_title = 'Logout';
 <main>
     <h1>You have been logout ! </h1>
     <h2>Thank You for using us :></h2>
-<h5>You will be redirected to the login page in 6.9 seconds</h5>
+<h5>You will be redirected to the login page in 2.0 seconds</h5>
 </main>
 <?php
 session_unset();
 session_destroy();
 
-header('Refresh: 6.9; MainPage.php'); 
+header('Refresh: 2.0; MainPage.php'); 
 ?>
